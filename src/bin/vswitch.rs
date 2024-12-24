e2643d01c9e77ac0688f121eb3a890ba6cac6086
danielvallance <danielvallance3435@gmail.com>
2024-12-24 19:31:25 +0000

    Begun implementing virtual switch
    Implemented handling and parsing of command line
    arguments in order to get port number

--- src/bin/vswitch.rs
+++ src/bin/vswitch.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+//! Virtual switch implementation
+//!
+//! This executable opens a UDP socket on the host,
+//! and handles the Ethernet frames sent to this
+//! socket as an Ethernet switch would
+//!
+//! Usage: vswitch <port>
+
+use std::{env, process::ExitCode};
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Expected 2 command line arguments and got {}", args.len());
+        eprintln!("Usage: vswitch <port>");
+        return ExitCode::FAILURE;
+    }
+
+    /* Get port number from command line argument */
+    let _port = match args[1].parse::<u32>() {
+        Ok(port) => port,
+        Err(e) => {
+            eprintln!("Got error: {}", e);
+            eprintln!("Could not parse {} as port number", args[1]);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    ExitCode::SUCCESS
 }
