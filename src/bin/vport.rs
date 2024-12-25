bf5f2760630754da9a06ffeae876d4c7a661e13c
danielvallance <danielvallance3435@gmail.com>
2024-12-25 16:03:56 +0000

    Handling and parsing of command line arguments for vport executable
    Decided to permit v4 and v6 addresses, as the underlying stack should
    not matter

--- src/bin/vport.rs
+++ src/bin/vport.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+//! Virtual switchport implementation
+//!
+//! This uses a TAP interface to send/receive
+//! the host's traffic to/from the vswitch
+//!
+//! Usage: vport <vswitch_ip> <vswitch_port>
+
+use std::{env, net::IpAddr, process::ExitCode};
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Expected 3 command line arguments and got {}", args.len());
+        eprintln!("Usage: vport <vswitch_ip> <vswitch_port>");
+        return ExitCode::FAILURE;
+    }
+
+    /* Get vswitch IP from command line argument */
+    let _vswitch_ip = match args[1].parse::<IpAddr>() {
+        Ok(ip) => ip,
+        Err(e) => {
+            eprintln!(
+                "Got error while parsing IP address from command line argument: {}",
+                e
+            );
+            eprintln!("Could not parse {} as IP address", args[1]);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    /* Get port number from command line argument */
+    let _port = match args[2].parse::<u32>() {
+        Ok(port) => port,
+        Err(e) => {
+            eprintln!("Got error while parsing port command line argument: {}", e);
+            eprintln!("Could not parse {} as port number", args[2]);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    ExitCode::SUCCESS
 }
